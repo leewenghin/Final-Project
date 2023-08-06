bc03fbc797af3434c87a9a519988f4f0c6050743
Lee Weng Hin <leewenghin1109@e.newera.edu.my>
2023-08-06 19:28:36 +0800

    chnage sidebar design

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,6 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -28,67 +29,79 @@
 
     <!-- Fontawesome -->
     <script src="https://kit.fontawesome.com/13427233db.js" crossorigin="anonymous"></script>
+
 </head>
+
 <body class="login_page_bng ">
     @if (Auth::check())
-        <div class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container d-flex justify-content-between">
-                <span style="font-size:30px;cursor:pointer" onclick="openNav()">&#9776; </span>
-                <div>
-                    <h4 class="m-auto">{{ Auth::user()->first_name.' '.Auth::user()->last_name }}</h4>
-                    <div class="d-flex justify-content-between">
-                        <div class="m-auto me-2">
-                            <img src="{{ asset(Auth::user()->GetAvatar()) }}" alt="" width="40px" height="40px" class="rounded-circle">
-                        </div>
-                        <form action="{{ route('logout') }}" method="POST" class="">
-                            @csrf
-                            <button type="submit" class="btn btn-danger text-white">Logout</button>
-                        </form>
+    <div class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+        <div class="container d-flex justify-content-between">
+            <span style="font-size:30px;cursor:pointer" onclick="openNav()">&#9776; </span>
+            <div>
+                <h4 class="m-auto">{{ Auth::user()->first_name.' '.Auth::user()->last_name }}</h4>
+                <div class="d-flex justify-content-between">
+                    <div class="m-auto me-2">
+                        <img src="{{ asset(Auth::user()->GetAvatar()) }}" alt="" width="40px" height="40px" class="rounded-circle">
                     </div>
+                    <form action="{{ route('logout') }}" method="POST" class="">
+                        @csrf
+                        <button type="submit" class="btn btn-danger text-white">Logout</button>
+                    </form>
                 </div>
             </div>
         </div>
-        <div id="mySidenav" class="sidenav">
+    </div>
+    <div id="mySidenav" class="sidenav">
+        <div class="sidenav-scrollable">
             <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
 
-            <a href="{{ route('home') }}" class="text-decoration-none"><h2 class="text-black text-center">e-Complaint</h2></a>
+            <a href="{{ route('home') }}" class="text-decoration-none justify-content-center">
+                <h2 class="text-center">e-Complaint</h2>
+            </a>
 
             <div class="d-flex justify-content-center my-3">
                 <img class="image_rounded" src="{{ asset(Auth::user()->GetAvatar()) }}" alt="profile.png" height="150px">
             </div>
 
-            <div class="text-center">
-                <h4>Name: {{ Auth::user()->first_name.' '.Auth::user()->last_name }}</h4>
+            <div class="list-group-top">
+                <h4 class="sidenav-profile text-center">Name: {{ Auth::user()->first_name.' '.Auth::user()->last_name }}</h4>
 
                 @if (Auth::user()->HasRole())
                 <div class="list-group hover_chg_text_clr">
-                        @if (Auth::user()->IsHelpDesk())
-                            <a data-bs-toggle="collapse" href="#collapseHelpdesk" role="button" aria-expanded="false" aria-controls="collapseHelpdesk" class="list-group-item list-group-item-action h5 @if(Request::is('help-desk/*')) active @endif">Help Desk</a>
-                            <div id="collapseHelpdesk" class="collapse">
-                                <a href="{{ route('helpdesk.dashboard') }}">Dashboard</a>
-                                <a href="{{ route('helpdesk.complaints.index') }}">All Complaints</a>
-                                <a href="{{ route('helpdesk.verified_complaints.index') }}">Ongoing Complaints</a>
-                            </div>
-                        @endif
-                        @if (Auth::user()->IsExecutive())
-                            <a href="{{ route('executive.dashboard') }}" class="list-group-item list-group-item-action h5 @if(Request::is('executive/*')) active @endif">Executive</a>
-                        @endif
-                        @if (Auth::user()->IsAdmin())
-                            <a data-bs-toggle="collapse" href="#collapseAdmin" role="button" aria-expanded="false" aria-controls="collapseAdmin" class="list-group-item list-group-item-action h5 @if(Request::is('administrator/*')) active @endif">Administrator</a>
-                        @endif
+                    @if (Auth::user()->IsHelpDesk())
+                    <a data-bs-toggle="collapse" href="#collapseHelpdesk" role="button" aria-expanded="false" aria-controls="collapseHelpdesk" class="list-group-item list-group-item-action h5 m-0 justify-content-between @if(Request::is('help-desk/*')) active @endif">
+                        Help Desk
+                        <i class="fa-solid fa-chevron-right fa-2xs"></i>
+                    </a>
+                    <div id="collapseHelpdesk" class="collapse">
+                        <a href="{{ route('helpdesk.dashboard') }}">Dashboard</a>
+                        <a href="{{ route('helpdesk.complaints.index') }}">Complaints</a>
+                        <a href="{{ route('helpdesk.verified_complaints.index') }}">Ongoing</a>
                     </div>
+                    @endif
+                    @if (Auth::user()->IsExecutive())
+                    <a href="{{ route('executive.dashboard') }}" class="list-group-item list-group-item-action h5 @if(Request::is('executive/*')) active @endif">Executive</a>
+                    @endif
+                    @if (Auth::user()->IsAdmin())
+                    <a data-bs-toggle="collapse" href="#collapseAdmin" role="button" aria-expanded="false" aria-controls="collapseAdmin" class="list-group-item list-group-item-action h5 justify-content-between @if(Request::is('administrator/*')) active @endif">
+                        Administrator
+                        <i class="fa-solid fa-chevron-right fa-2xs"></i>
+                    </a>
+                    @endif
+                </div>
                 @endif
             </div>
 
             @if (Auth::user()->HasRole())
-                @if (Auth::user()->IsAdmin())
-                    <div id="collapseAdmin" class="collapse">
-                        <a href="{{ route('users.index') }}">User Profile List</a>
-                        <a href="{{ route('departments.index') }}">Department List</a>
-                    </div>
-                @endif
+            @if (Auth::user()->IsAdmin())
+            <div id="collapseAdmin" class="collapse">
+                <a href="{{ route('users.index') }}">User Profile List</a>
+                <a href="{{ route('departments.index') }}">Department List</a>
+            </div>
+            @endif
             @endif
         </div>
+    </div>
     @endif
 
     <div id="main" class="py-4">
@@ -96,9 +109,9 @@
     </div>
 
     @if (Auth::check())
-        <div>
-            @include('layouts.footer')
-        </div>
+    <div>
+        @include('layouts.footer')
+    </div>
     @endif
 </body>
 
